aca1cc9998955d14cc308c0f0723386e5e598805
tty <1074395132@qq.com>
2021-06-17 14:06:20 +0800

    提交留学生创业园的页面

--- src/pages/yt_district/index.vue
+++ src/pages/yt_district/index.vue
@@ -3,13 +3,13 @@
   <div>
     <div class="supermap" id="supermap">
       <!-- 超图 -->
-      <img src="../../assets/img/bg.jpg" title ="超图的位置">
-      <!-- <yt-CesiumCom @func="getclick"></yt-CesiumCom> -->
+      <!-- <img src="../../assets/img/bg.jpg" title ="超图的位置"> -->
+      <yt-CesiumCom @func="getclick"></yt-CesiumCom>
     </div>
     <!-- 区的页面组件 -->
-    <v-district v-show="isshow == false"></v-district>
+    <leave-garden v-if="isshow"></leave-garden>
     <!-- 建筑 的组件 -->
-    <leave-garden v-show="isshow == true"></leave-garden>
+    <v-district v-else></v-district>
   </div>
 </template>
 
@@ -21,8 +21,8 @@ export default {
   data() {
     return {
       // 显示隐藏区组件与建筑组件 true显示区的组件
-      isshow: true,
-      // isshow: false,
+      // isshow: true,
+      isshow: false,
     };
   },
   components: {
